733bafb50b2f8c1ed34f52f39e31e3c169c6f662
benl95 <beno_1995@hotmail.com>
2020-10-21 10:14:16 +0200

    move file to functional-programming

---
+++ functional-programming/index.js
@@ -0,0 +1,10 @@
+console.log('script loaded')
+
+// fetch("data/Survey_Information_Design_clean-parsed.json")
+// .then(response => response.json())
+// .then(json => console.log(json));
+
+// const surveyAnswers = data
+// console.log(surveyAnswers)
+
+// console.log(surveyAnswers[3].hoeveelheidOomsenTantes)
