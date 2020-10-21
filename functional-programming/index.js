a052047f7c42aa19552a32355ea71d18c9206a07
benl95 <beno_1995@hotmail.com>
2020-10-21 11:58:37 +0200

    create index.js

--- functional-programming/index.js
+++ functional-programming/index.js
@@ -1,5 +1,10 @@
 console.log('script loaded')
 
+// Methode 2: Stop alle data in een string variable
+
+// Stap 1: Put all data in a string variable in the data folder using the template literal sign ``
+// Stap 2: Load that file in html before you load this file so it's available
+
 // fetch("data/Survey_Information_Design_clean-parsed.json")
 // .then(response => response.json())
 // .then(json => console.log(json));
