25e0289d9142540f320561b3598ef0205d1e6452
benl95 <beno_1995@hotmail.com>
2020-10-22 13:44:35 +0200

    Create for loop to push answers into array and convert values

--- functional-programming/script.js
+++ functional-programming/script.js
@@ -1,2 +1,43 @@
-const surveyData = data()
-console.log(surveyData)
+// Store data in variable
+const surveyAnswers = data()
+
+// Store search action in variable and create array
+let columnName = 'lievelingsWeekdag'
+let questionAnswers = []
+
+// Loop through answers and convert day value to actual day
+for (day of surveyAnswers) {
+    switch(day[columnName]) {
+        case '1':
+            day[columnName] = 'Monday'
+            break;
+        case '2':
+            day[columnName] = 'Tuesday'
+            break;
+        case '3':
+            day[columnName] = 'Wednesday'
+            break;
+        case '4':
+            day[columnName] = 'Thursday'
+            break;
+        case '5':
+            day[columnName] = 'Friday'
+            break;
+        case '6': 
+            day[columnName] = 'Saturday'
+            break;
+        case '7':
+            day[columnName] = 'Sunday'
+            break;
+        default: 
+            day[columnName] ='No input'
+    }
+    questionAnswers.push(day[columnName])
+}
+
+// Log array
+console.log(questionAnswers)
+
+
+
+
