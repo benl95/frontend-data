47ed46bbfaa33fb8fd5bb0b44027495a3dd3a594
benl95 <beno_1995@hotmail.com>
2020-10-23 15:44:57 +0200

    remove comments

--- functional-programming/script.js
+++ functional-programming/script.js
@@ -34,46 +34,5 @@ const getQuestionForAnswers = surveyAnswers.map((column) => {
 
 console.log(getQuestionForAnswers)
 
-// Store search action in variable and create array
-/* let columnName = 'lievelingsWeekdag'
-let questionAnswers = [] */
-
-/* questionAnswers.push(getQuestionForAnswers)
-console.log(questionAnswers) */
-
-// Loop through answers and convert day value to actual day
-/* for (day of surveyAnswers) {
-    switch(day[columnName]) {
-        case '1':
-            day[columnName] = 'Monday'
-            break;
-        case '2':
-            day[columnName] = 'Tuesday'
-            break;
-        case '3':
-            day[columnName] = 'Wednesday'
-            break;
-        case '4':
-            day[columnName] = 'Thursday'
-            break;
-        case '5':
-            day[columnName] = 'Friday'
-            break;
-        case '6': 
-            day[columnName] = 'Saturday'
-            break;
-        case '7':
-            day[columnName] = 'Sunday'
-            break;
-        default: 
-            day[columnName] ='No input'
-    }
-    questionAnswers.push(day[columnName])
-} */
-
-// Log array
-/* console.log(questionAnswers) */
-
-
 
 
