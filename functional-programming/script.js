141f213214f219c6852df4b7430c2c82d535b26b
benl95 <beno_1995@hotmail.com>
2020-10-23 15:36:49 +0200

    use map method to search through array and convert values using switch statement

--- functional-programming/script.js
+++ functional-programming/script.js
@@ -1,12 +1,48 @@
 // Store data in variable
 const surveyAnswers = data()
 
+// Search array of data with map method and use a switch statement to convert the given answers to actual days.
+const getQuestionForAnswers = surveyAnswers.map((column) => {
+    switch(column.lievelingsWeekdag) {
+        case '1':
+            column.lievelingsWeekdag = 'Monday'
+            break
+        case '2':
+            column.lievelingsWeekdag = 'Tuesday'
+            break
+        case '3':
+            column.lievelingsWeekdag = 'Wednesday'
+            break
+        case '4':
+            column.lievelingsWeekdag = 'Thursday'
+            break
+        case '5':
+            column.lievelingsWeekdag = 'Friday'
+            break
+        case '6':
+            column.lievelingsWeekdag = 'Saturday'
+            break
+        case '7':
+            column.lievelingsWeekdag = 'Sunday'
+            break
+        case '':
+            column.lievelingsWeekdag = 'No input'
+            break
+    }
+    return column.lievelingsWeekdag
+})
+
+console.log(getQuestionForAnswers)
+
 // Store search action in variable and create array
-let columnName = 'lievelingsWeekdag'
-let questionAnswers = []
+/* let columnName = 'lievelingsWeekdag'
+let questionAnswers = [] */
+
+/* questionAnswers.push(getQuestionForAnswers)
+console.log(questionAnswers) */
 
 // Loop through answers and convert day value to actual day
-for (day of surveyAnswers) {
+/* for (day of surveyAnswers) {
     switch(day[columnName]) {
         case '1':
             day[columnName] = 'Monday'
@@ -33,10 +69,10 @@ for (day of surveyAnswers) {
             day[columnName] ='No input'
     }
     questionAnswers.push(day[columnName])
-}
+} */
 
 // Log array
-console.log(questionAnswers)
+/* console.log(questionAnswers) */
 
 
 
