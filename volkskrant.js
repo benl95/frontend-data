bc19fa34be79e4b44bae42803b37c71c432c9ce4
benl95 <beno_1995@hotmail.com>
2020-11-03 13:18:04 +0100

    Wrote reduce function to count unique input values

--- volkskrant.js
+++ volkskrant.js
@@ -1,25 +1,38 @@
-// Endpoint which returns amount of registered vehicles on postal code including fuel usage
-// Source: Live coding college Laurens
-const endpoint = 'https://opendata.rdw.nl/resource/8wbe-pu7d.json'
-const selectedColumns = ['postcode', 'brandstof', 'aantal']
+// Paste this at end of url to increase limit: ?$limit=2000
+const url = 'https://opendata.rdw.nl/resource/8ys7-d773.json?$limit=100000'
 
-getData(endpoint)
+// Retrieve data from url
+getData(url)
     .then(data => {
         console.log('all data: ', data)
-        const postalCodeArray = filterData(data, selectedColumns[0])
-        const fuelUsageArray = filterData(data, selectedColumns[1])
-        const registeredAmountArray = filterData(data, selectedColumns[2])
-        console.log(registeredAmountArray)
-        console.log(postalCodeArray)
-        console.log(fuelUsageArray)
+        // Get fuel usage of all registered cars
+        const fuelUsage = filterData(data, 'brandstof_omschrijving')
+        console.log(fuelUsage)
+        const fuelOccurences = countOccurences(fuelUsage)
+        console.log(fuelOccurences)
     })
 
-async function getData (endpoint) {
-    const response = await fetch(endpoint)
+// Function to fetch data from url
+async function getData (url) {
+    const response = await fetch(url)
     const data = await response.json()
     return data
 }
 
+// Function to filter data
 function filterData (dataArray, column) {
-    return dataArray.map(result => result[column])
+    return dataArray.map(result => result[column])  
 }  
+
+// Reduce function to count unique input values
+// Source: https://stackoverflow.com/questions/5667888/counting-the-occurrences-frequency-of-array-elements
+function countOccurences(arr) {
+    return arr.reduce(function(acc, curr) {
+        if (acc[curr]) {
+            acc[curr] += 1
+        } else {
+            acc[curr] = 1
+        }
+        return acc
+    }, [])
+}
