d05832057570f628873ddd82972991ff4f360a89
benl95 <beno_1995@hotmail.com>
2020-11-10 16:40:33 +0100

    Filter first dataset on columns and store into var carInfo

--- volkskrant.js
+++ volkskrant.js
@@ -1,5 +1,5 @@
 // URL endpoint
-const endpoints = ['https://opendata.rdw.nl/resource/m9d7-ebf2.json?$limit=100000', 'https://opendata.rdw.nl/resource/8ys7-d773.json?$limit=100000'];
+const endpoints = ['https://opendata.rdw.nl/resource/m9d7-ebf2.json?$limit=10000', 'https://opendata.rdw.nl/resource/8ys7-d773.json?$limit=10000'];
 
 // Specific columns
 const columnNames = ['kenteken', 'brandstof_omschrijving', 'co2_uitstoot_gecombineerd', 'emissiecode_omschrijving', 'voertuigsoort', 'merk', 'handelsbenaming'];
@@ -12,10 +12,14 @@ getData(endpoints)
 		// All data from both datasets
 		console.log('all data: ', data);
 
-		const licensedVehicles = data[0];
-
 		// Filter desired columns from dataset using filterData function
-		const licensePlateNumber = filterData(data[1], columnNames[0]);
+		// First dataset
+		const licensePlateNumberFirst = filterData(data[0], columnNames[0]);
+		const vehicleType = filterData(data[0], columnNames[4]);
+		const brand = filterData(data[0], columnNames[5]);
+		const tradeName = filterData(data[0], columnNames[6]);
+		// Second dataset
+		const licensePlateNumberSecond = filterData(data[1], columnNames[0]);
 		const fuelUsage = filterData(data[1], columnNames[1]);
 		const co2Emission = filterData(data[1], columnNames[2]);
 		const emissionCode = filterData(data[1], columnNames[3]);
@@ -27,10 +31,12 @@ getData(endpoints)
 		const convertedEmissionCode = removeNaN(parsedEmissionCode);
 
 		// Merge arrays into one array
-		let carArray = mergeArrays(licensePlateNumber, fuelUsage, convertedCo2Emission, convertedEmissionCode);
-		console.log(carArray);
+		let carInfo = mergeFirstArray(licensePlateNumberFirst, vehicleType, brand, tradeName);
+		console.log(carInfo);
+		let fuelInfo = mergeSecondArray(licensePlateNumberSecond, fuelUsage, convertedCo2Emission, convertedEmissionCode);
+		console.log(fuelInfo);
 
-		let merged = merge(carArray, licensedVehicles);
+		mergeDatasets(fuelInfo, carInfo);
 	});
 
 // Function to fetch data from url and parse to json
@@ -50,12 +56,13 @@ function filterData(dataArray, column) {
 	return dataArray.map((result) => result[column]);
 }
 
-function merge(a, b) {
+// Function to find matches based on id and nest into array when match is found
+function mergeDatasets(a, b) {
 	let carArray = a;
 	let licensedVehicles = b;
 
 	const result = carArray.map((carArray, i) => {
-		const carInfo = licensedVehicles.find((carInfo) => carArray.id === carInfo.kenteken);
+		const carInfo = licensedVehicles.find((carInfo) => carArray.id === carInfo.id);
 		carArray.carInfo = carInfo;
 		console.log(i);
 		return carArray;
@@ -65,7 +72,16 @@ function merge(a, b) {
 
 // Function to merge arrays into one array using map function
 // With help from Jonah and Vincent, thank you!
-function mergeArrays(mainArray, array1, array2, array3) {
+function mergeFirstArray(mainArray, array1, array2, array3) {
+	return mainArray.map((val, idx) => ({
+		id: val,
+		vehicleType: array1[idx],
+		brand: array2[idx],
+		tradeName: array3[idx],
+	}));
+}
+
+function mergeSecondArray(mainArray, array1, array2, array3) {
 	return mainArray.map((val, idx) => ({
 		id: val,
 		fuel: array1[idx],
